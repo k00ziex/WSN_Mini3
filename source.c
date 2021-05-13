7004d2d7b77a14bf89068e6c3d3dad281d47c76b
tobiassandoelund <tobiassandoelund@gmail.com>
2021-05-13 11:14:05 +0200

    Source1 kinda done

--- source.c
+++ source.c
@@ -17,11 +17,22 @@ PROCESS(broadcast_process, "broadcast_process");
 AUTOSTART_PROCESSES(&broadcast_process);
 /*---------------------------------------------------------------------------*/
 
-static char payload[100];
-static uint8_t payloadSize; 
+static const uint8_t tempData[] = { 26, 22, 25, 30, 20, 16, 27, 20, 24, 17, 16, 17, 28, 19, 21, 25, 28, 27, 20, 18, 25, 15, 19, 25, 24, 20, 30, 16, 26, 17, 21, 28, 20, 16, 23, 19, 17, 26, 
+                            22, 17, 16, 18, 17, 20, 30, 23, 18, 21, 25, 23, 24, 28, 21, 26, 17, 15, 20, 28, 21, 30, 24, 20, 29, 29, 28, 19, 19, 27, 26, 22, 30, 22, 30, 22, 23, 27, 
+                            21, 22, 17, 27, 24, 25, 27, 18, 28, 29, 19, 26, 20, 19, 17, 25, 23, 28, 19, 17, 21, 22, 27, 17};
+
+static int tempDataIndex = 0;
+static uint8_t packageId = 1; 
+
+uint8_t GetNextSourceData() 
+{
+  uint8_t val = tempData[tempDataIndex];
+  ++packageId;
+  ++tempDataIndex;
+  return val;
+}
+
 static linkaddr_t aggmote_address; 
-
-
 void nullnet_send(SourceData* data)
 {
     // Set ptr type
@@ -29,12 +40,13 @@ void nullnet_send(SourceData* data)
     nullnet_len = sizeof(SourceData);
     // Copy data into buffer
     memcpy(nullnet_buf, data, sizeof(SourceData));
+    NETSTACK_NETWORK.output(&aggmote_address); 
+    LOG_INFO("\nData sent\n");
 }
 
 PROCESS_THREAD(broadcast_process, ev, data)
 {
   static struct etimer timer;
-  static uint8_t packageId = 1; 
   // Pick aggmote based on even ID.
   if(node_id % 2 == 0) 
   {
@@ -49,21 +61,27 @@ PROCESS_THREAD(broadcast_process, ev, data)
   while (1) {
     LOG_INFO("broadcast_process broadcasting:\n");
 
-    etimer_set(&timer, 3 * CLOCK_SECOND);
+    etimer_set(&timer, 1 * CLOCK_SECOND);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer) );
     
+    uint8_t val = GetNextSourceData();
+
     // struct to send
     struct SourceData sd = {
-      69,
-      1,
-      2
+      node_id,
+      packageId,
+      val
     };
-  
-    nullnet_send(&sd);
 
-    NETSTACK_NETWORK.output(&aggmote_address); 
-    LOG_INFO("\nData sent\n");
+    
+    nullnet_send(&sd);
+    
     etimer_reset(&timer);
+    // End of life for our mote.
+    if(tempDataIndex >= 99) 
+    {
+      break;
+    }
   }
 
   LOG_INFO("broadcast_process ending\n");
