bcca9291494ec96af861fdf33d0a29e6dd0f23e0
Simon Jensen <simonzhjensen@gmail.com>
2021-05-13 14:12:21 +0200

    Update sink.c
    Just changed the msgEventCallback in the sink to print the necessary received data

--- sink.c
+++ sink.c
@@ -2,7 +2,10 @@
 #include "net/nullnet/nullnet.h"
 #include "net/netstack.h"
 #include "sys/log.h"
+#include "AggNetwork.h"
 #include "sys/energest.h"
+#include "net/packetbuf.h"
+
 #define LOG_MODULE "receiver_process"
 #define LOG_LEVEL LOG_LEVEL_DBG
 
@@ -59,12 +62,25 @@ void energestMeasurement(char c, int8_t txPower, int runTime) {
 
 
 void msgEventCallback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) {
-  LOG_INFO("Received message! with len: %d \n", len);
+  
+  struct AggData ad;
+  memcpy(&ad, data, sizeof(ad));
+
+  LOG_INFO("I received\n");
+  LOG_INFO("AggregatorID: %d\n", ad.AggregatorId);
+  LOG_INFO("PackageId: %d\n", ad.PackageId);
+  //print float skal bruges til at printe float værdi.
+  LOG_INFO("Min Val: %d\n", ad.Min);
+  LOG_INFO("Max Val: %d\n", ad.Max);
+  LOG_INFO("Median: %d\n", ad.Median);
+  LOG_INFO("Number of Measurements: %d\n", ad.NumMeasurements);
+  
+  /* LOG_INFO("Received message! with len: %d \n", len);
   int i = 0;
   for(; i < len; i++) {
     //uint8_t c = *(uint8_t*)(data+i);
     LOG_INFO("RECEIVED: %c \n", *(char*)((uint8_t*)(data+i)));
-  }
+  } */
 }
 
 
