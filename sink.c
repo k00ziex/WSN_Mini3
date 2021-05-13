1918c42a3c19a9fbb830fbabef71d1b62a63785b
Simon Jensen <simonzhjensen@gmail.com>
2021-05-13 12:46:54 +0200

    Update sink.c
    Updated the energest measurement method

--- sink.c
+++ sink.c
@@ -16,16 +16,44 @@ void printFloat(float f) {
     printf("%ld.%02u, ",A, (unsigned int) fraction);
 }
 
-void energestMeasurement() {
+
+//Char c = om der er tale om transmit reading eller receive, t for transmit
+//txPower sættes i tilfælde af der er tale om transmitting
+//Runtime er hvorlangtid den har stået og arbejdet dette vil i bedste tilfælde være timerens værdi. 
+void energestMeasurement(char c, int8_t txPower, int runTime) {
   energest_flush(); // kan muligvis godt være at dette skal ændres så vi udregner fra den forrige måling.
-  uint16_t CpuMeasurement = energest_type_time(ENERGEST_TYPE_CPU);
-  uint16_t LpmMeasurement = energest_type_time(ENERGEST_TYPE_LPM);
-  uint16_t TxMeasurement = energest_type_time(ENERGEST_TYPE_TRANSMIT);
-  uint16_t RxMeasurement = energest_type_time(ENERGEST_TYPE_LISTEN);
+  uint16_t cpuMeasurement = energest_type_time(ENERGEST_TYPE_CPU);
+  uint16_t lpmMeasurement = energest_type_time(ENERGEST_TYPE_LPM);
+  uint16_t txMeasurement = energest_type_time(ENERGEST_TYPE_TRANSMIT);
+  uint16_t rxMeasurement = energest_type_time(ENERGEST_TYPE_LISTEN);
+  float currentConsumption;
+
+  if(c == 't' && (txPower >= -25 && txPower <= 0)) {
+
+    switch(txPower) {
+      case -25:
+        currentConsumption = 0.085
+        break;
+      case -15:
+        currentConsumption = 0.099
+        break;
+      case -10:
+        currentConsumption = 0.11
+        break;
+      case -5:
+        currentConsumption = 0.14
+        break;
+      default:
+        currentConsumption = 0.174
+    }
+
+  } else {
+    currentConsumption = 0.197
+  }
 
   //19,7 mA i receive mode, 3 V - det kan godt være dette lige skal undersøges nærmere da jeg var liiidt i tvivl om værdierne.
-  float energyConsumption = (CpuMeasurement * 0.197 * 3) / ENERGEST_SECOND;
-  float dutyCycle = (TxMeasurement + RxMeasurement) / (CpuMeasurement + LpmMeasurement);
+  float energyConsumption = (cpuMeasurement * currentConsumption * 3) / (ENERGEST_SECOND * runTime);
+  float dutyCycle = (txMeasurement + rxMeasurement) / (cpuMeasurement + lpmMeasurement);
 }
 
 
